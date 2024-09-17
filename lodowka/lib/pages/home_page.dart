0ca6acc3e1c9df5ba5ddee1da566a26538cc295d
SimonFine <61289026+SimonFine@users.noreply.github.com>
2024-09-17 15:18:04 +0200

    Add bottom navigation bar widget with two destinations

--- lodowka/lib/pages/home_page.dart
+++ lodowka/lib/pages/home_page.dart
@@ -10,9 +10,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const<Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.add_outlined),
+            label: 'Add Product',
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
