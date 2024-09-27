bb89b1039eb171588a227cfde7b2d22a37aa7a20
abraxxxas <alex.lewandowska@interia.pl>
2024-09-27 21:31:26 +0200

    Add appbar in fridge

--- lodowka/lib/pages/fridge.dart
+++ lodowka/lib/pages/fridge.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 class Fridge extends StatefulWidget {
   const Fridge({super.key});
@@ -10,6 +11,35 @@ class Fridge extends StatefulWidget {
 class _FridgeState extends State<Fridge> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 25),
+          child: IconButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (contex) => HomePage()));
+            },
+            icon: Icon(Icons.arrow_back, 
+              color: Color(0xFF1D1808),
+              size: 38,)
+          ),
+        ),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 25),
+          child: Text(
+            'Your Fridge',
+            style: TextStyle(
+              color: Color(0xFF1D1808),
+              fontSize: 36,
+            ),
+          ),
+        ),
+        backgroundColor: const Color(0xFFFDFDFD),
+        scrolledUnderElevation: 0,
+      ),
+    );
   }
 }
